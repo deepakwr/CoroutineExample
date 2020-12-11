4ecf0d328bc6ce73e8a50a2b48d77cf9f4bd074e
deepak.mac <deepak3314@gmail.com>
2020-12-11 17:31:53 +0530

    Exploring suspend functions and lifecylescope.

--- app/src/main/java/com/deepak/coroutineexample/MainActivity.kt
+++ app/src/main/java/com/deepak/coroutineexample/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.deepak.coroutineexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+//        Suspend function 'doNetworkCall' should be called only from a coroutine or another suspend functio
+//        doNetworkCall();
 
         GlobalScope.launch {
             /*
@@ -28,18 +32,56 @@ class MainActivity : AppCompatActivity() {
             The reason because once the main thread is removed or stopped, the other threads connected in the application scope are removed.
              */
 
-            delay(5000L);
+
+            delay(5000L);   //suspend function interally, can be called within CoroutineScope. Here delay is added on this thread.
             Log.i(TAG,"Thread name within Coroutine : ${Thread.currentThread().name}")
             val threadName = Thread.currentThread().name;
 
             launch(Dispatchers.Main){
                 text_view_message.text = "Main Activity : ${threadName}";
             }
+
+            val netWorkcall = doNetworkCall();  //Delay added of 3 seconds
+            val netWorkcallAgain = doNetworkCallAgain(); //Delay added of 3 seconds again
+
+            Log.i(TAG,"MainActivity netWorkcall : ${netWorkcall}")
+            Log.i(TAG,"MainActivity netWorkcallAgain : ${netWorkcallAgain}")
         }
+
+
+
+
+        btn_start_activity.setOnClickListener {
+            lifecycleScope.launch {
+                while(true){
+                    delay(1000L)
+                    Log.i(TAG,"MainActivity onStartActivity :   Still Running..")
+                }
+            }
+
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity,SecondActivity::class.java).also{
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
+
 
         Log.i(TAG,"MainActivity onCreate :    Thread name within Coroutine : ${Thread.currentThread().name}")
         text_view_message.text = "Main Activity : ${Thread.currentThread().name}";
 
+    }
 
+
+    suspend fun doNetworkCall():String{
+        delay(3000L);
+        return "This is the answer - doNetworkCall";
+    }
+
+    suspend fun doNetworkCallAgain():String{
+        delay(3000L);
+        return "This is the answer - doNetworkCallAgain";
     }
 }
